f522636c5e05b15dcb6d9d1b0ba417d5f3592b10
Alberto Ricatto s309141 <s309141@studenti.polito.it>
2024-01-24 09:23:21 +0100

    fixed typo

---
+++ launch_scripts/run_extension_part1.sh
@@ -0,0 +1,14 @@
+
+# Random experiments to test extention
+#source ./launch_scripts/random_no_binarization.sh cartoon 0.4
+#source ./launch_scripts/DA_no_binarization.sh cartoon conv1
+
+source ./launch_scripts/random_no_binarization.sh sketch 0.4
+#source ./launch_scripts/DA_no_binarization.sh sketch conv1
+
+source ./launch_scripts/random_no_binarization.sh photo 0.4
+#source ./launch_scripts/DA_no_binarization.sh photo conv1
+
+
+
+read -n1 -r -p "Press any key to continue..."
